bb3b8f4e533d4c3ebf5f169009b5ef787d31b5fa
Ulises Julca <ulises@adventistas.pe>
2023-11-02 22:41:34 -0500

    clientes

--- water_services_api/apps/operation/serializers/Client.py
+++ water_services_api/apps/operation/serializers/Client.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
 
+from water_services_api.apps.configuration.serializers.Person import PersonBasicSerializer
 from water_services_api.apps.operation.models.Client import Client
 
 
 class ClientSerializer(serializers.ModelSerializer):
+    person = PersonBasicSerializer(many=False, read_only=True)
+
     class Meta:
         model = Client
-        fields = '__all__'
+        fields = ('id','person', 'client_type_id', 'situation_id', 'plan_id', 'block', 'lot', 'is_retired',
+                  'is_finalized_contract', 'is_active')
