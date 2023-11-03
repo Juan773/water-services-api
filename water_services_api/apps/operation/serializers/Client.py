38461ca0da8e3a1e345e0ae2ae7bda50e8268463
Ulises Julca <ulises@adventistas.pe>
2023-11-02 23:01:49 -0500

    clientes

--- water_services_api/apps/operation/serializers/Client.py
+++ water_services_api/apps/operation/serializers/Client.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from water_services_api.apps.configuration.serializers.Person import PersonBasicSerializer
+from water_services_api.apps.core.HostValues import HostValues
 from water_services_api.apps.operation.models.Client import Client
 
 
@@ -11,3 +12,45 @@ class ClientSerializer(serializers.ModelSerializer):
         model = Client
         fields = ('id', 'person', 'person_id', 'client_type_id', 'situation_id', 'plan_id', 'block', 'lot',
                   'is_retired', 'is_finalized_contract', 'is_active')
+
+
+class ClientBasicSerializer(serializers.ModelSerializer):
+    logo = serializers.SerializerMethodField()
+    thumbnail = serializers.SerializerMethodField()
+    full_name = serializers.SerializerMethodField()
+    person_id = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Client
+        fields = ('id', 'person_id', 'full_name', 'thumbnail', 'logo')
+
+    def get_person_id(self, q):
+        return q.person.id
+
+    def get_full_name(self, q):
+        return q.person.full_name
+
+    def get_logo(self, q):
+        foto = ""
+        request = self.context.get('request')
+        if request:
+            dominio = HostValues.domain(request)
+            if q.person.logo:
+                foto = dominio + q.person.logo.url
+            else:
+                foto = ""
+        return foto
+
+    def get_thumbnail(self, q):
+        foto = ""
+        request = self.context.get('request')
+        if request:
+            dominio = HostValues.domain(request)
+            if q.person.logo:
+                if q.person.thumbnail:
+                    foto = dominio + q.person.thumbnail.url
+                else:
+                    foto = ""
+            else:
+                foto = ""
+        return foto
--- water_services_api/apps/operation/serializers/Payment.py
+++ water_services_api/apps/operation/serializers/Payment.py
@@ -1,13 +1,16 @@
 from rest_framework import serializers
 
 from water_services_api.apps.operation.models.Payment import Payment, PaymentDetail
+from water_services_api.apps.operation.serializers.Client import ClientBasicSerializer
 
 
 class PaymentSerializer(serializers.ModelSerializer):
+    client = ClientBasicSerializer(many=False, read_only=True)
+
     class Meta:
         model = Payment
         fields = (
-            'id', 'client', 'date', 'plan', 'payment_method', 'serie_payment', 'nro_payment', 'nro_operation',
+            'id', 'client', 'client_id', 'date', 'plan_id', 'payment_method_id', 'serie_payment', 'nro_payment', 'nro_operation',
             'total', 'user', 'is_active')
 
 
