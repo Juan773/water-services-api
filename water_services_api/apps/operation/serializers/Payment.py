785c6dc665788e4ed399db82e9214cb44fb7e731
Ulises Julca <ulises@adventistas.pe>
2023-11-07 13:48:39 -0500

    filter quotas

--- water_services_api/apps/operation/serializers/Payment.py
+++ water_services_api/apps/operation/serializers/Payment.py
@@ -4,17 +4,18 @@ from water_services_api.apps.configuration.serializers.PaymentMethod import Paym
 from water_services_api.apps.operation.models.Payment import Payment, PaymentDetail
 from water_services_api.apps.operation.serializers.Client import ClientBasicSerializer
 from water_services_api.apps.operation.serializers.Plan import PlanBasicSerializer
+from water_services_api.apps.operation.serializers.Quota import QuotaBasicSerializer
 
 
 class PaymentSerializer(serializers.ModelSerializer):
     client = ClientBasicSerializer(many=False, read_only=True)
-    plan = PlanBasicSerializer(many=False, read_only=True)
+    quota = QuotaBasicSerializer(many=False, read_only=True)
     payment_method = PaymentMethodSerializer(many=False, read_only=True)
 
     class Meta:
         model = Payment
         fields = (
-            'id', 'client', 'client_id', 'date', 'plan', 'plan_id', 'payment_method', 'payment_method_id',
+            'id', 'client', 'client_id', 'date', 'quota', 'quota_id', 'payment_method', 'payment_method_id',
             'serie_payment', 'nro_payment', 'nro_operation', 'total', 'user', 'is_active')
 
 
